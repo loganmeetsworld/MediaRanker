4407f4b11617230d7dbd492f22beb7210272c09f
Logan McDonald <loganmcdona11@gmail.com>
2015-12-01 16:58:04 -0800

    Finished with tests

--- spec/support/medium_controller_spec.rb
+++ spec/support/medium_controller_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 RSpec.shared_examples "medium controller" do
-	let(:name_string) { model.to_s.downcase}
-	let(:plur_name) { name_string + "s" }
-	let(:media) { name_string.to_sym }
+	let(:sing_name) { model.to_s.downcase}
+	let(:plur_name) { sing_name + "s" }
+	let(:media) { sing_name.to_sym }
   let(:media_params) { { media => { name: "name" } } }
   let(:bad_params) { { media => {} } }
 
@@ -62,28 +62,60 @@ RSpec.shared_examples "medium controller" do
 		end
 	end
 
-	describe "GET 'edit'" do
-		context "valid params" do 
-		end
-
-		context "invalid params" do 
-		end
-	end
-
 	describe "PATCH 'update'" do
-		context "valid params" do 
+		before :each do 
+			@media = model.create(name: "test")
 		end
 
-		context "invalid params" do 
+		context "selecting the edit button" do
+			it "redirects to the individual show page" do
+				patch :update, { id: @media.id }.merge(media_params)
+				expect(subject).to redirect_to polymorphic_path(@media)
+			end
+
+			it "updates the record" do 
+				patch :update, { id: @media.id }.merge(media_params)
+				@media.reload
+				expect(@media.name).to eq media_params[media][:name]
+			end
 		end
 	end
 
 	describe "DELETE 'destroy'" do
-		context "selecting delete button"
+		before :each do 
+			@media = model.create(name: "name")
+		end
+
+		context "selecting delete button" do
+			it "redirects to the individual show page" do
+				delete :destroy, id: @media.id
+				expect(subject).to redirect_to polymorphic_path(plur_name)
+			end
+
+			it "should be able to delete media" do 
+				expect { delete :destroy, { id: @media.id } }.to change(model, :count).by(-1)
+			end
+		end
 	end
 
 	describe "POST 'vote'" do 
-		context "selecting upvote button" do 
+		before :each do 
+			@media = model.create(name: "name")
+			request.env["HTTP_REFERER"] = polymorphic_path(@media)
+		end
+
+		context "selecting upvote button" do
+			it "adds one to the rank" do 
+				post :vote, id: @media.id
+				@media.reload 
+				expect(@media.rank).to eq 1
+			end
+
+			it "stays on the same page after vote" do
+				post :vote, id: @media.id
+				@media.reload
+				expect(subject).to redirect_to polymorphic_path(@media)
+			end
 		end
 	end
 
